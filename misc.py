d749c6c10ef53c66de4c62eea34f3e0388e6f078
Michael Douglas <mdb529@gmail.com>
2022-06-20 12:16:16 -0500

    replacing cached files again

--- misc.py
+++
@@ -1,8 +0,0 @@
-import os
-from google.cloud import bigquery
-from google.oauth2 import service_account
-
-os.environ["BQ_SA_CREDENTIALS"] = "service_account.Credentials.from_service_account_file('/Users/michaeldouglas/bq-dbt-metrics-app-serviceaccount.json')"
-os.environ["BQ_PROJECT"] = 'dbt-metrics-dw'
-
-print(os.environ["BQ_SA_CREDENTIALS"])
